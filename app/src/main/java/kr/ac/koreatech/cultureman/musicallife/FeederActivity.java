87465030b46bb337e6ee40c5fb40d44e8aca0d4f
choiseonmun <ch002717@gmail.com>
2018-11-03 18:51:50 +0900

    Init Project

---
+++ app/src/main/java/kr/ac/koreatech/cultureman/musicallife/FeederActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.cultureman.musicallife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeederActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feeder);
+    }
+}
