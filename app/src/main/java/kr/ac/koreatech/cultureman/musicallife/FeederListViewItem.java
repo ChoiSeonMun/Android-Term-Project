ad355763d5695b0215319c632c059b91df55fbf5
Jihye <jihye3519@gmail.com>
2018-11-16 16:50:14 +0900

    Constructed FeederListViewItem Class

--- app/src/main/java/kr/ac/koreatech/cultureman/musicallife/FeederListViewItem.java
+++ app/src/main/java/kr/ac/koreatech/cultureman/musicallife/FeederListViewItem.java
@@ -1,4 +1,34 @@
 package kr.ac.koreatech.cultureman.musicallife;
 
+import android.graphics.drawable.Drawable;
+
 public class FeederListViewItem {
+
+    private Drawable musicalImage ;
+    private String musicalTitle ;
+    private String musicalPlace;
+    private String musicalDate ;
+
+    public void setImage(Drawable icon) {
+        musicalImage = icon ;
+    }
+    public void setTitle(String title) {
+        musicalTitle = title ;
+    }
+    public void setPlace(String place) { musicalPlace = place ; }
+    public void setDate(String date) {
+        musicalDate = date ;
+    }
+
+    public Drawable getImage() {
+        return this.musicalImage ;
+    }
+    public String getTitle() {
+        return this.musicalTitle ;
+    }
+    public String getPlace() { return this.musicalPlace ; }
+    public String getDate() {
+        return this.musicalDate ;
+    }
+
 }
