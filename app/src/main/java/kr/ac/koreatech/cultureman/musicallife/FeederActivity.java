abf799cc147448afb62d7c5a1a84565ebeaf8f72
Leechansol <lcs9603@koreatech.ac.kr>
2018-11-25 18:59:06 +0900

    add_ScrollView

--- app/src/main/java/kr/ac/koreatech/cultureman/musicallife/FeederActivity.java
+++ app/src/main/java/kr/ac/koreatech/cultureman/musicallife/FeederActivity.java
@@ -1,7 +1,10 @@
 package kr.ac.koreatech.cultureman.musicallife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FeederActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class FeederActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeder);
+
+        Button b = (Button)findViewById(R.id.button1);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FeederActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
